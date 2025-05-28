2a28e8e40dfd77f890ebffbf068f5604a593e02e
mira_yu <mira_yu@croxlink.com>
2025-05-28 18:06:06 +0800

    feat: update dependencies and add testing support; enhance Tailwind CSS configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   srcDir: 'src/',
-  modules: ['@nuxtjs/tailwindcss'],
-})
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon','@vee-validate/nuxt','@nuxt/test-utils/module'],
+  icon: {
+    mode: 'css',
+    cssLayer: 'base'
+  },
+  vite: {
+    test: {
+      globals: true,
+      environment: 'jsdom',
+    },
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,45 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: '#fffdf4',
+        background: '#fcf9eb',
         'brand-blue': '#1e40af', // 自訂顏色
         primary: {
           DEFAULT: '#ffb901',
-          light: '#fefbea',
-          dark: '#461a01',
+          light: '#fcf9eb',
+          dark: '#6b4d00',
+        },
+        accent: {
+          DEFAULT: '#45b8af',
+          light: '#9dd2cd',
+          dark: '#1b8684',
         },
         secondary: {
-          DEFAULT: '#444444',
-          light: '#d7d3d0',
-          dark: '#292524',
+          DEFAULT: '#f59127',
+          light: '#ffb86a',
+          dark: '#eb6f4d',
         },
+        neutral: {
+          DEFAULT: '#949292',
+          light: '#d7d3d0',
+          dark: '#4f4e4e',
+        },
+      },
+      screens: {
+        'xs': '480px', // 新增一個更小的斷點
       },
       fontFamily: {
         'custom': ['"Noto Sans TC"', 'sans-serif'], // 自訂字型
+      },
+      fontSize: {
+        'xs': '0.75rem',
+        'sm': '0.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
       },
     },
   },
